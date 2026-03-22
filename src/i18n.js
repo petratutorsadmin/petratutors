c72685b0f6f275ff235b7075bf141d401af60984
yball2022 <141697705+yball2022@users.noreply.github.com>
2026-03-23 01:35:49 +0900

    feat: persist i18next language choice on reload

--- src/i18n.js
+++ src/i18n.js
@@ -1,20 +1,21 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 import enTranslation from './locales/en.json';
 import jaTranslation from './locales/ja.json';
 
 i18n
+    .use(LanguageDetector)
     .use(initReactI18next)
     .init({
         resources: {
             en: { translation: enTranslation },
             ja: { translation: jaTranslation }
         },
-        lng: 'en', // default language
         fallbackLng: 'en',
         interpolation: {
-            escapeValue: false // react already safes from xss
+            escapeValue: false
         }
     });
 
